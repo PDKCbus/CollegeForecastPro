3d271a4d22089cda2b7de984f3db4a3716b1a88a
Peter Kinzelman <peterkinzelman@Peters-MBP.lan>
2025-08-03 20:58:48 -0400

    Add deployment scripts for production

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Add global styles for dark mode
-document.documentElement.classList.add('dark');
-
-createRoot(document.getElementById("root")!).render(<App />);
